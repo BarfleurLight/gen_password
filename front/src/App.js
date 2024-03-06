89c84a42de53092f7cfc27c605e4b2d1c3d53c3a
Mark <obrishtigoogl@gmail.com>
2024-03-06 09:05:17 +0300

    add BackButton and pages

--- front/src/App.js
+++ front/src/App.js
@@ -4,27 +4,45 @@ import Form from './components/form';
 import Head from './components/head';
 import {useTelegram} from './utils/tg/tg';
 import {defoltConsts} from './utils/consts'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function App() {
   
+  const [swiper, setSwiper] = React.useState(null);
+
   // Подключение MainButton
   const {tg} = useTelegram();
 
-  const sendDataToTelegram = () => {
-    tg.close();
+  const mainBut = () => {
+    if (swiper.activeIndex === 0) {
+      swiper.slideNext();
+      tg.BackButton.isVisible(true)
+      } else {
+        // swiper.slidePrev();
+        tg.close();
+      }
+  }
+
+  const backBut  = () => {
+    tg.BackButton.isVisible(false);
+    swiper.slidePrev();
   }
 
   useEffect(() => {
     tg.ready();
-    tg.MainButton.setParams({text: 'Создать шаблон'});
-    tg.MainButton.show();
     tg.expand();
-    tg.onEvent('mainButtonClicked', sendDataToTelegram)
-    return () => {
-      tg.offEvent('mainButtonClicked', sendDataToTelegram)
-    }
-  }, [sendDataToTelegram])
-  
+    tg.MainButton.setParams({text: 'Далее'});
+    tg.MainButton.show();
+    tg.MainButton.onClick(mainBut);
+    tg.BackButton.onClick(backBut);
+    
+    // tg.onEvent('mainButtonClicked', sendDataToTelegram)
+    // return () => {
+    //   tg.offEvent('mainButtonClicked', sendDataToTelegram)
+    // }
+  }, [mainBut, backBut])
+
   //Стандартные параметры паароля
   const {defolt_pass} = defoltConsts();
 
@@ -37,10 +55,20 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Head pass={Password}/>
-      <Form onSavePass={savePassword} pass={Password}/>
-    </div>
+  <div className="App">
+    <Swiper className="mySwiper" 
+            onSwiper={(s) => {
+            setSwiper(s);
+            }}>
+      <SwiperSlide virtualIndex={0}>
+        <Head pass={Password}/>
+        <Form onSavePass={savePassword} pass={Password}/>
+      </SwiperSlide>
+      <SwiperSlide virtualIndex={1}>        
+        <Head pass={Password}/>
+        </SwiperSlide>
+    </Swiper>
+  </div>
   );
 }
 
