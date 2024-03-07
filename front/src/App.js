20cc0088100fd509b848ff56cce12390926eefb6
Mark <obrishtigoogl@gmail.com>
2024-03-07 11:35:41 +0300

    add scrol form

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,9 @@ import {useTelegram} from './utils/tg/tg';
 import {defoltConsts} from './utils/consts'
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import 'swiper/css/free-mode';
+import 'swiper/css/scrollbar';
+import { FreeMode, Scrollbar, Mousewheel } from 'swiper/modules';
 
 function App() {
   
@@ -15,10 +18,11 @@ function App() {
   // Подключение MainButton
   const {tg} = useTelegram();
 
+
   const mainBut = () => {
     if (swiper.activeIndex === 0) {
       swiper.slideNext();
-      tg.BackButton.show()
+      tg.BackButton.show();
       } else {
         // swiper.slidePrev();
         tg.close();
@@ -36,7 +40,7 @@ function App() {
     tg.MainButton.setParams({text: 'Далее'});
     tg.MainButton.show();
     tg.MainButton.onClick(mainBut);
-    tg.BackButton.onClick(backBut);
+    // tg.BackButton.onClick(backBut);
     
   }, [mainBut, backBut])
 
@@ -61,13 +65,25 @@ function App() {
             setSwiper(s);
             }}>
       <SwiperSlide virtualIndex={0}>
-        <Head pass={Password}/>
-        <Form onSavePass={savePassword} pass={Password}/>
+        <Head pass={Password} slot="container-start"/>
+        <Swiper 
+          direction={'vertical'}
+          slidesPerView={'auto'}
+          freeMode={true}
+          mousewheel={true}
+          nested={true}
+          modules={[FreeMode, Scrollbar, Mousewheel]}
+          className="test"
+        >
+          <SwiperSlide >
+            <Form onSavePass={savePassword} pass={Password} />
+          </SwiperSlide>
+        </Swiper>
       </SwiperSlide>
       <SwiperSlide virtualIndex={1}>        
         <Head pass={Password}/>
         <Name />
-        </SwiperSlide>
+      </SwiperSlide>
     </Swiper>
   </div>
   );
