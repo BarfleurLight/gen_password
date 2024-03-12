276d40c4a161ba0a57fa98fa05fe5feeeaf905eb
Варя Бандуро <varesenina@gmail.com>
2024-03-12 20:50:48 +0300

    fix example size font

--- front/src/components/example/index.js
+++ front/src/components/example/index.js
@@ -1,14 +1,13 @@
 import styles from './style.module.css'
 import generateString from '../../utils/gen/gen.js'
-import React, { useEffect } from 'react';
 
 const Example = (props) => {
-  const test = props.pass['length']
-  
-
+  const length = props.pass['length']
+  const width = Math.max(0, 410 - window.innerWidth) 
+  const size = 40 - Math.max(0, (Math.log2(Math.max(1, (-14 + width / 18) + length)) * (5 + width / 100)))
 
   return (
-    <div id='test' className={styles.example} style= {{fontSize: `calc( ${40 - Math.max(0, (Math.log2(-15 + test) * 5)) }px )` }} >
+    <div id='test' className={styles.example} style= {{fontSize: size } } >
       {generateString(props.pass)}
     </div>
   )
