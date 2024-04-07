efece591ce2d2e1a4f7aa2407587474fe2abcb92
Варя Бандуро <varesenina@gmail.com>
2024-04-07 13:10:24 +0300

    Fix button

--- front/src/App.js
+++ front/src/App.js
@@ -18,21 +18,41 @@ function App() {
   // Подключение MainButton
   const {tg} = useTelegram();
 
-
   const mainBut = () => {
     if (swiper.activeIndex === 0) {
       swiper.slideNext();
-      tg.BackButton.show();
-      } else {
-        // swiper.slidePrev();
-        tg.close();
-      }
+    } else {
+      tg.close();
+    }
   }
 
   const backBut  = () => {
-    tg.BackButton.hide();
-    swiper.slidePrev();
+    if (swiper.activeIndex === 0) {
+      tg.BackButton.hide();
+    } else {
+      tg.BackButton.show();
+      swiper.slidePrev();
+    }
+    // swiper.slidePrev();
   }
+  // const mainBut1 = () => {
+  //   tg.close()
+  //     swiper.slidePrev();
+  //     tg.BackButton.show();
+  // }
+
+  // const test = () => {
+  //   if (swiper.activeIndex === 0) {
+  //     tg.MainButton.onClick();
+  //     tg.BackButton.hide();
+  //   } else {
+  //     tg.MainButton.setParams({text: 'Готово'});
+  //     tg.MainButton.onClick(mainBut1);
+  //     tg.BackButton.show();
+  //   }
+  // }
+
+
 
   useEffect(() => {
     tg.ready();
@@ -41,7 +61,6 @@ function App() {
     tg.MainButton.show();
     tg.MainButton.onClick(mainBut);
     tg.BackButton.onClick(backBut);
-    
   }, [])
 
   //Стандартные параметры паароля
