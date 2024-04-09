114317207ce935d0330c5b04cf14e68ed83a0a71
Варя Бандуро <varesenina@gmail.com>
2024-04-09 16:16:40 +0300

    test revork

--- front/src/components/button/index.js
+++ front/src/components/button/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from 'react';
+import { useEffect, useCallback, useState } from 'react';
 import { useSwiper } from "swiper/react";
 import { useTelegram } from '../../utils/tg/tg';
 
@@ -8,10 +8,31 @@ const Button = (props) => {
   const {tg} = useTelegram();
   const swiper = useSwiper();
 
-  const id = tg.initDataUnsafe.user?.id;
+  // const id = useState(tg.initDataUnsafe.user?.id);
+  const name_pass= useState(props.name);
+  const password = useState(props.pass);
 
-  const name_pass = createContext(props.name)
-  const password = createContext(props.pass)
+  const [activeIndex, setActiveIndex] = useState(swiper.activeIndex)
+
+
+  // const id = tg.initDataUnsafe.user?.id;
+
+  // const name_pass = props.name
+  // const password = props.pass
+
+  // const data = {
+  //   'id': tg.initDataUnsafe.user?.id,
+  //   'name_pass': props.name,
+  //   'password': props.pass
+  // }
+  const updateData = useCallback(() => {
+    const data = {
+        'id': tg.initDataUnsafe.user?.id,
+        'name_pass': name_pass,
+        'password': password
+      }
+      return data
+  }, [tg, name_pass, password]);
 
 
   const changeColor = () => {
@@ -23,67 +44,119 @@ const Button = (props) => {
   };
 
 
-  const mainBut = () => {
+  // const mainBut = () => {
+  //     if (swiper.activeIndex === 1) {
+  //         if (name_pass === '') {
+  //           changeColor();
+  //         } else {
+  //           sendData();
+  //           tg.close();
+  //         }
+  //     } else {
+  //       swiper.slideNext("speed:", 900);
+  //     }
+  // };
+
+  // MainButton
+  useEffect(() => {
+    const data = updateData()
+    const sendData = () => {
+      if (data.id === undefined) {
+        return console.log('Errror get_id')
+      }
+
+      console.log(data)
+  
+      var response = fetch('https://obrishti.ddns.net/webhook/template', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+      })
+  
+      response.catch((err) => {
+        console.log(err.message);
+     });
+  
+    }
+
+    const mainBut = () => {
       if (swiper.activeIndex === 1) {
-          if (name_pass.Provider === '') {
-            changeColor();
-          } else {
-            sendData();
-            tg.close();
-          }
+        if (data.name_pass === '') {
+          changeColor();
+        } else {
+          sendData();
+          tg.close();
+        }
       } else {
         swiper.slideNext("speed:", 900);
       }
-  };
-
-  const backBut = () => {
-      swiper.slidePrev("speed:", 900);
-      tg.BackButton.hide();
-  };
-
-  const updateButton = () => {
-      if (swiper.activeIndex === 1) {
-        tg.BackButton.show()
-        tg.MainButton.onClick(mainBut);
-      } else {
-        tg.MainButton.onClick(mainBut);
-        tg.BackButton.hide()
-      }
-  }
-
-
-  useEffect(() => {
+    };
     tg.MainButton.onClick(mainBut);
-    tg.BackButton.onClick(backBut);
-    swiper.on('slideChange', updateButton);
-  }, [tg, swiper]);
+  }, [tg, swiper, updateData, activeIndex]);
+
+  // BackButton
+useEffect(() => {
+  const backBut = () => {
+    console.log('test0')
+    swiper.slidePrev("speed:", 900);
+    tg.BackButton.hide();
+    };
+  if (activeIndex === 1) {
+    console.log('test1')
+    tg.BackButton.show()
+    tg.MainButton.onClick(backBut);
+  } else {
+    console.log('test2')
+    tg.BackButton.hide()
+  }
+}, [tg, swiper, activeIndex]);
+
+
+  // const updateButton = () => {
+  //     if (swiper.activeIndex === 1) {
+  //       console.log(name_pass)
+  //       tg.BackButton.show()
+  //       tg.MainButton.onClick(mainBut);
+  //     } else {
+  //       tg.MainButton.onClick(mainBut);
+  //       tg.BackButton.hide()
+  //     }
+  // }
+  
+  useEffect(() => {
+    swiper.on('slideChange',() => {
+      setActiveIndex(swiper.activeIndex)
+    });
+    }, [swiper]);
 
   
-  const sendData = () => {
-    if (id === undefined) {
-      return console.log('Errror get_id')
-    }
+  // const sendData = () => {
+  //   if (id === undefined) {
+  //     return console.log('Errror get_id')
+  //   }
 
-    const data = {
-      'id': id,
-      'name_pass': name_pass.Provider,
-      'password': password.Provider
-    }
-    console.log(data)
+  //   const data = {
+  //     'id': id,
+  //     'name_pass': name_pass,
+  //     'password': password
+  //   }
+  //   console.log(data)
 
-    var response = fetch('https://obrishti.ddns.net/webhook/template', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(data)
-    })
+  //   var response = fetch('https://obrishti.ddns.net/webhook/template', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json;charset=utf-8'
+  //     },
+  //     body: JSON.stringify(data)
+  //   })
 
-    response.catch((err) => {
-      console.log(err.message);
-   });
+  //   response.catch((err) => {
+  //     console.log(err.message);
+  //  });
 
-  }
+  // }
 }
 
 
